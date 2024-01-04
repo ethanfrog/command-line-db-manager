776afa2d6bc2a12a065f04d0545a5cda2c94217f
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-01-04 01:10:16 -0500

    Fixed SQL schema syntax

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,16 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-  id INT PRIMARY KEY NOT NULL,
-  dept_name VARCHAR(30)
+  id INT NOT NULL,
+  dept_name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
   id INT PRIMARY KEY NOT NULL,
   title VARCHAR(30),
   salary DECIMAL,
+  department_id INT,
   FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
@@ -19,7 +21,8 @@ CREATE TABLE employees (
   id INT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
   FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-  --Set to NULL when there is no manager
   FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
