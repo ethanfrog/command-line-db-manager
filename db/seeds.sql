9f9ab06f33611bc5fc0ed3a610e40579320f4278
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-01-04 20:54:45 -0500

    Updated seeds.sql to incorporate managers

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,10 +5,11 @@ VALUES ("Accounting"),
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Secretary", 50.0, 001),
         ("Janitor", 40.0, 001),
-        ("Salesman", 60.0, 002);
+        ("Salesman", 60.0, 002),
+        ("Manager", 90.0, 002);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Bob", "Smith", 002, NULL),
-        ("Jane", "Doe", 001, NULL),
-        ("Mark", "Johnson", 003, NULL),
-        ("Paula", "Clark", 003, NULL);
+VALUES ("Bob", "Smith", 004, NULL),
+        ("Jane", "Doe", 001, 001),
+        ("Mark", "Johnson", 002, NULL),
+        ("Paul", "Clark", 003, 001);
